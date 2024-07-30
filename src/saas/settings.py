d37954e6a7351f74135845affc4d5da7f7871dd7
Volha Dziatko <volha10.dziatko@gmail.com>
2024-07-30 15:15:00 +0200

    add dotenv

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-48j&-tqzvdvm67517j&9iw8^848+-j2yn4tr(+lutfa)(d!&fb"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG")
+print(f"DEBUG={DEBUG}")
 
 ALLOWED_HOSTS = []
 
